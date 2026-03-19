995b24042c20036046996d9f9d8ed64e7a672276
Urwa_Kausar <urwakausar55@gmail.com>
2026-03-20 00:45:42 +0500

    Add files via upload

---
+++ Pythoin-Practice(lists).py
@@ -0,0 +1,18 @@
+marks=[56,78,35,67,35]
+print(marks)
+print(marks[4])#index/place 
+student=["urwa",5.2,"islamabad",20]# we can store multiple type of data in the list
+student[0]="raahima"# we can change index in list which we cannot do in strings
+print(student)
+#slicing
+print(marks[1:3])
+marks.append(78)
+print(marks)
+marks.sort()
+print(marks)
+marks.sort(reverse=True)
+print(marks)
+marks.reverse()
+print(marks)
+marks.insert(1,89)
+print(marks)
