1d2e56bc3dcc2689a6f869d0f6afa16e4959aa98
Urwa_Kausar <urwakausar55@gmail.com>
2026-03-20 00:46:43 +0500

    Add files via upload

---
+++ Python-Practice(sets).py
@@ -0,0 +1,11 @@
+sett={1,2,3}
+sett2={1,2,4,5,6}
+print(sett)
+sett.add(4)
+print(sett)
+sett.remove(2)
+print(sett)
+#print(sett.clear())# length my 0 show huga print(len(sett))
+print(sett.pop())
+print(sett.union(sett2))
+print(sett.intersection(sett2))
