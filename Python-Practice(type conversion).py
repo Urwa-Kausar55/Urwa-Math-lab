920ac1f8c49a02628a31223a15ad4ed1e844f022
Urwa_Kausar <urwakausar55@gmail.com>
2026-03-20 00:44:16 +0500

    Add files via upload

---
+++ Python-Practice(type conversion).py
@@ -0,0 +1,10 @@
+#type conversion
+a=2
+b=5.7
+sum=a+b
+print(sum)
+#type conversion
+a=int("34")# if we don't write int here it will give error because "34" is a string 
+b=5.7
+sum=a+b
+print(sum)
